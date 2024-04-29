f08c6bf43a339ed7751dac0750a1e7242d87e70e
blazittx <info@doruksasmaz.com>
2024-04-29 09:20:21 +0300

    Electron Updated

--- src/index.js
+++ src/index.js
@@ -27,9 +27,6 @@ const createWindow = () => {
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-
   // Emitted when the window is closed.
   mainWindow.on('closed', () => {
     mainWindow = null;
