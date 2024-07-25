2453d942b20b65e511d45a9c880e9436cef77421
blazittx <info@doruksasmaz.com>
2024-07-25 03:04:24 +0300

    Size settings done

--- src/index.js
+++ src/index.js
@@ -1,94 +1,121 @@
-// index.js
+const { app, BrowserWindow, ipcMain } = require("electron");
+const { autoUpdater } = require("electron-updater");
+const fs = require("fs");
+const path = require("path");
+const os = require("os");
+const { download } = require("electron-dl");
+const extract = require("extract-zip");
+const { fetchGames } = require("./js/database");
+const oracledb = require("oracledb");
+const { exec } = require("child_process");
+const axios = require("axios");
 
-const { app, BrowserWindow, ipcMain } = require('electron');
-const { autoUpdater } = require('electron-updater');
-const fs = require('fs');
-const path = require('path');
-const os = require('os');
-const { download } = require('electron-dl');
-const extract = require('extract-zip');
-const { fetchGames } = require('./js/database');
-const oracledb = require('oracledb');
-const { exec } = require('child_process');
-const axios = require('axios');
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 let allowResize = false;
 
 autoUpdater.autoDownload = false;
 autoUpdater.autoInstallOnAppQuit = true;
 
-// Define the path to the DiabolicalLauncher directory
-const diabolicalLauncherPath = path.join(os.homedir(), 'AppData', 'Local', 'Diabolical Launcher');
-const versionFilePath = gameId => path.join(diabolicalLauncherPath, `${gameId}-version.json`);
-const settingsFilePath = path.join(diabolicalLauncherPath, 'settings.json');
+const diabolicalLauncherPath = path.join(
+  os.homedir(),
+  "AppData",
+  "Local",
+  "Diabolical Launcher"
+);
+const versionFilePath = (gameId) =>
+  path.join(diabolicalLauncherPath, `${gameId}-version.json`);
+const settingsFilePath = path.join(diabolicalLauncherPath, "settings.json");
+
+// Default settings
+const defaultSettings = {
+  windowSize: { width: 1280, height: 720 },
+  theme: "dark",
+  language: "en",
+};
+
+function loadSettings() {
+  try {
+    if (fs.existsSync(settingsFilePath)) {
+      const data = fs.readFileSync(settingsFilePath);
+      return JSON.parse(data);
+    }
+  } catch (error) {
+    console.error("Error reading settings:", error);
+  }
+  return defaultSettings;
+}
+
+function saveSettings(settings) {
+  try {
+    fs.writeFileSync(settingsFilePath, JSON.stringify(settings, null, 2));
+  } catch (error) {
+    console.error("Error saving settings:", error);
+  }
+}
 
 const createWindow = () => {
-  // Create the browser window.
-  let windowSize = { width: 1280, height: 720 };
-
-  if (fs.existsSync(settingsFilePath)) {
-    try {
-      const data = fs.readFileSync(settingsFilePath);
-      windowSize = JSON.parse(data);
-    } catch (error) {
-      console.error('Error reading window size:', error);
-    }
-  }
+  const settings = loadSettings();
 
   mainWindow = new BrowserWindow({
-    width: windowSize.width,
-    height: windowSize.height,
+    width: settings.windowSize.width,
+    height: settings.windowSize.height,
     frame: false,
-    icon: 'path/to/your/icon.ico',
+    icon: "path/to/your/icon.ico",
     webPreferences: {
       contextIsolation: true,
       nodeIntegration: true,
       enableRemoteModule: false,
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, "preload.js"),
     },
-    resizable: true  // This prevents the window from being resizable
+    resizable: true,
   });
 
-  // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', () => {
+  mainWindow.on("closed", () => {
     mainWindow = null;
   });
 
   mainWindow.center();
 
-  mainWindow.on('will-resize', (e) => {
+  mainWindow.on("will-resize", (e) => {
     if (!allowResize) {
       e.preventDefault();
     }
   });
 
-  // Adjusted usage
-  mainWindow.webContents.on('did-finish-load', async () => {
+  mainWindow.webContents.on("did-finish-load", async () => {
     autoUpdater.checkForUpdates();
-    pingDatabase('https://diabolical.studio'); // Use your site's URL here
+    pingDatabase("https://diabolical.studio");
     showMessage(`Checking for updates...`);
 
-    // Check for game updates on load
     const installedGames = getInstalledGames();
     for (const gameId of installedGames) {
       await checkForGameUpdates(gameId);
     }
   });
 
-  // Ping the site every 10 seconds to check status
   setInterval(() => {
-    pingDatabase('https://diabolical.studio'); // Use your site's URL here
-  }, 60000); // Adjust the interval as needed
+    pingDatabase("https://diabolical.studio");
+  }, 60000);
+
+  mainWindow.on("close", () => {
+    const [width, height] = mainWindow.getSize();
+    settings.windowSize = { width, height };
+    saveSettings(settings);
+  });
 };
 
+ipcMain.handle("get-settings", () => {
+  return loadSettings();
+});
 
-ipcMain.handle('get-window-size', () => {
+ipcMain.handle("update-settings", (event, newSettings) => {
+  const settings = { ...loadSettings(), ...newSettings };
+  saveSettings(settings);
+});
+
+ipcMain.handle("get-window-size", () => {
   if (mainWindow) {
     const { width, height } = mainWindow.getBounds();
     return { width, height };
@@ -96,12 +123,14 @@ ipcMain.handle('get-window-size', () => {
   return { width: 1280, height: 720 };
 });
 
-ipcMain.on('set-window-size-and-center', (event, width, height) => {
+ipcMain.on("set-window-size-and-center", (event, width, height) => {
   if (mainWindow) {
     allowResize = true;
     mainWindow.setSize(width, height);
     mainWindow.center();
-    fs.writeFileSync(settingsFilePath, JSON.stringify({ width, height }));
+    const settings = loadSettings();
+    settings.windowSize = { width, height };
+    saveSettings(settings);
     allowResize = false;
   } else {
     console.log("Main window is not accessible.");
@@ -109,24 +138,23 @@ ipcMain.on('set-window-size-and-center', (event, width, height) => {
 });
 
 function pingDatabase(url) {
-  axios.get(url)
-    .then(response => {
-      // Check for a successful response status code (200-299)
+  axios
+    .get(url)
+    .then((response) => {
       if (response.status >= 200 && response.status < 300) {
         console.log(`Website ${url} is up! Status Code: ${response.status}`);
-        mainWindow.webContents.send('db-status', 'rgb(72, 216, 24)'); // Green, site is up
+        mainWindow.webContents.send("db-status", "rgb(72, 216, 24)");
       } else {
         console.log(`Website ${url} returned status code ${response.status}`);
-        mainWindow.webContents.send('db-status', 'red'); // Red, site has issues
+        mainWindow.webContents.send("db-status", "red");
       }
     })
-    .catch(error => {
+    .catch((error) => {
       console.error(`Error checking ${url}: ${error}`);
-      mainWindow.webContents.send('db-status', 'red'); // Red, site is down or unreachable
+      mainWindow.webContents.send("db-status", "red");
     });
 }
 
-// A function to handle showing messages and sending them to the renderer
 function showMessage(message) {
   console.log("showMessage trapped");
   console.log(message);
@@ -135,10 +163,6 @@ function showMessage(message) {
   }
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-/*New Update Available*/
 autoUpdater.on("update-available", (info) => {
   showMessage(`Update available. Download Started...`);
   let pth = autoUpdater.downloadUpdate();
@@ -149,14 +173,12 @@ autoUpdater.on("update-not-available", (info) => {
   showMessage(`Launcher version:`);
 });
 
-/*Download Completion Message*/
 autoUpdater.on("update-downloaded", (info) => {
   showMessage(`Update downloaded. Restarting...`);
   autoUpdater.quitAndInstall();
 });
 
-// Listen for an 'check-for-updates' message from the renderer
-ipcMain.on('check-for-updates', () => {
+ipcMain.on("check-for-updates", () => {
   autoUpdater.checkForUpdates();
 });
 
@@ -164,42 +186,39 @@ autoUpdater.on("error", (info) => {
   showMessage(info);
 });
 
-app.on('ready', function () {
+app.on("ready", function () {
   createWindow();
 });
 
-ipcMain.on('close-window', () => {
+ipcMain.on("close-window", () => {
   if (mainWindow) {
     mainWindow.close();
   }
 });
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (mainWindow === null) {
     createWindow();
   }
 });
 
-if (require('electron-squirrel-startup')) {
+if (require("electron-squirrel-startup")) {
   app.quit();
 }
 
-ipcMain.handle('load-games', async () => {
+ipcMain.handle("load-games", async () => {
   try {
     return await fetchGames();
   } catch (error) {
@@ -208,23 +227,30 @@ ipcMain.handle('load-games', async () => {
   }
 });
 
-ipcMain.handle('load-html', async (event, filePath) => {
+ipcMain.handle("load-html", async (event, filePath) => {
   const fullPath = path.join(app.getAppPath(), filePath);
-  return fs.promises.readFile(fullPath, 'utf8').catch(error => console.error(error));
+  return fs.promises
+    .readFile(fullPath, "utf8")
+    .catch((error) => console.error(error));
 });
 
 async function getLatestGameVersion(gameId) {
-  const apiUrl = 'https://objectstorage.eu-frankfurt-1.oraclecloud.com/n/frks8kdvmjog/b/DiabolicalGamesStorage/o/';
+  const apiUrl =
+    "https://objectstorage.eu-frankfurt-1.oraclecloud.com/n/frks8kdvmjog/b/DiabolicalGamesStorage/o/";
 
   try {
     const response = await fetch(apiUrl);
     const data = await response.json();
 
     const versions = data.objects
-      .map(obj => obj.name)
-      .filter(name => name.startsWith(`${gameId}/Versions/Build-StandaloneWindows64-`))
-      .map(name => {
-        const versionMatch = name.match(/Build-StandaloneWindows64-(\d+\.\d+\.\d+)\.zip$/);
+      .map((obj) => obj.name)
+      .filter((name) =>
+        name.startsWith(`${gameId}/Versions/Build-StandaloneWindows64-`)
+      )
+      .map((name) => {
+        const versionMatch = name.match(
+          /Build-StandaloneWindows64-(\d+\.\d+\.\d+)\.zip$/
+        );
         return versionMatch ? versionMatch[1] : null;
       })
       .filter(Boolean)
@@ -235,7 +261,7 @@ async function getLatestGameVersion(gameId) {
 
     return { latestVersion, latestVersionUrl };
   } catch (error) {
-    console.error('Failed to fetch the latest game version:', error);
+    console.error("Failed to fetch the latest game version:", error);
     throw error;
   }
 }
@@ -244,19 +270,27 @@ async function checkForGameUpdates(gameId) {
   try {
     const { latestVersion } = await getLatestGameVersion(gameId);
 
-    let installedVersion = '0.0.0';
+    let installedVersion = "0.0.0";
     if (fs.existsSync(versionFilePath(gameId))) {
-      const versionData = JSON.parse(fs.readFileSync(versionFilePath(gameId), 'utf8'));
-      installedVersion = versionData.version || '0.0.0';
+      const versionData = JSON.parse(
+        fs.readFileSync(versionFilePath(gameId), "utf8")
+      );
+      installedVersion = versionData.version || "0.0.0";
     }
 
     if (latestVersion !== installedVersion) {
-      mainWindow.webContents.send('update-available', { gameId, updateAvailable: true });
+      mainWindow.webContents.send("update-available", {
+        gameId,
+        updateAvailable: true,
+      });
     } else {
-      mainWindow.webContents.send('update-available', { gameId, updateAvailable: false });
+      mainWindow.webContents.send("update-available", {
+        gameId,
+        updateAvailable: false,
+      });
     }
   } catch (error) {
-    console.error('Error checking for game updates:', error);
+    console.error("Error checking for game updates:", error);
   }
 }
 
@@ -266,51 +300,65 @@ async function extractZip(zipPath, gameId, event) {
     await extract(zipPath, { dir: extractPath });
     fs.unlinkSync(zipPath);
 
-    const executablePath = path.join(extractPath, 'StandaloneWindows64.exe');
-    event.sender.send('download-complete', gameId, executablePath);
+    const executablePath = path.join(extractPath, "StandaloneWindows64.exe");
+    event.sender.send("download-complete", gameId, executablePath);
     return executablePath;
   } catch (error) {
-    console.error('Extraction error:', error);
-    event.sender.send('download-error', gameId, 'Extraction failed');
+    console.error("Extraction error:", error);
+    event.sender.send("download-error", gameId, "Extraction failed");
   }
 }
 
-ipcMain.on('download-game', async (event, gameId, platform = 'StandaloneWindows64') => {
-  try {
-    const { latestVersion, latestVersionUrl } = await getLatestGameVersion(gameId);
+ipcMain.on(
+  "download-game",
+  async (event, gameId, platform = "StandaloneWindows64") => {
+    try {
+      const { latestVersion, latestVersionUrl } = await getLatestGameVersion(
+        gameId
+      );
 
-    const gameUrl = latestVersionUrl;
+      const gameUrl = latestVersionUrl;
 
-    const dl = await download(BrowserWindow.getFocusedWindow(), gameUrl, {
-      directory: diabolicalLauncherPath,
-      onProgress: (progress) => {
-        const progressData = {
-          gameId: gameId,
-          percentage: progress.percent
-        };
-        console.log("Sending download progress:", progressData);
-        event.sender.send('download-progress', progressData);
-      }
-    });
+      const dl = await download(BrowserWindow.getFocusedWindow(), gameUrl, {
+        directory: diabolicalLauncherPath,
+        onProgress: (progress) => {
+          const progressData = {
+            gameId: gameId,
+            percentage: progress.percent,
+          };
+          console.log("Sending download progress:", progressData);
+          event.sender.send("download-progress", progressData);
+        },
+      });
 
-    await extractZip(dl.getSavePath(), gameId, event);
+      await extractZip(dl.getSavePath(), gameId, event);
 
-    fs.writeFileSync(versionFilePath(gameId), JSON.stringify({ version: latestVersion }));
+      fs.writeFileSync(
+        versionFilePath(gameId),
+        JSON.stringify({ version: latestVersion })
+      );
 
-    mainWindow.webContents.send('update-available', { gameId, updateAvailable: false });
-  } catch (error) {
-    console.error('Download or Extraction error:', error);
-    event.sender.send('download-error', gameId, error.message);
+      mainWindow.webContents.send("update-available", {
+        gameId,
+        updateAvailable: false,
+      });
+    } catch (error) {
+      console.error("Download or Extraction error:", error);
+      event.sender.send("download-error", gameId, error.message);
+    }
   }
-});
+);
 
-ipcMain.on('open-game', (event, gameId) => {
+ipcMain.on("open-game", (event, gameId) => {
   const gamePath = path.join(diabolicalLauncherPath, gameId);
-  const executablePath = path.join(gamePath, 'StandaloneWindows64.exe');
+  const executablePath = path.join(gamePath, "StandaloneWindows64.exe");
 
   if (!fs.existsSync(executablePath)) {
     console.error(`Executable not found at path: ${executablePath}`);
-    event.sender.send('game-launch-error', `Executable not found at path: ${executablePath}`);
+    event.sender.send(
+      "game-launch-error",
+      `Executable not found at path: ${executablePath}`
+    );
     return;
   }
 
@@ -321,9 +369,12 @@ ipcMain.on('open-game', (event, gameId) => {
       console.error(`Failed to open game: ${error.message}`);
       console.error(`stdout: ${stdout}`);
       console.error(`stderr: ${stderr}`);
-      event.sender.send('game-launch-error', `Failed to open game: ${error.message}`);
+      event.sender.send(
+        "game-launch-error",
+        `Failed to open game: ${error.message}`
+      );
     } else {
-      console.log('Game launched successfully');
+      console.log("Game launched successfully");
     }
   });
 });
@@ -335,8 +386,12 @@ function getInstalledGames() {
       return [];
     }
 
-    const files = fs.readdirSync(diabolicalLauncherPath, { withFileTypes: true });
-    const installedGames = files.filter(dirent => dirent.isDirectory()).map(dirent => dirent.name);
+    const files = fs.readdirSync(diabolicalLauncherPath, {
+      withFileTypes: true,
+    });
+    const installedGames = files
+      .filter((dirent) => dirent.isDirectory())
+      .map((dirent) => dirent.name);
     return installedGames;
   } catch (error) {
     console.error("Failed to list installed games:", error);
@@ -344,6 +399,6 @@ function getInstalledGames() {
   }
 }
 
-ipcMain.handle('get-installed-games', async (event) => {
+ipcMain.handle("get-installed-games", async (event) => {
   return getInstalledGames();
 });
