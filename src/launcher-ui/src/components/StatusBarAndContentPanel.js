9f324d37f4261244382f60ba2000605bb076dc49
blazittx <info@doruksasmaz.com>
2025-04-27 02:24:27 +0200

    fix: border visibility

--- src/launcher-ui/src/components/StatusBarAndContentPanel.js
+++ src/launcher-ui/src/components/StatusBarAndContentPanel.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const StatusBarAndContentPanel = ({children}) => {
-    return (<div className={"w-full flex flex-col gap-3 overflow-hidden"}>
+    return (<div className={"w-full flex flex-col gap-3"}>
         {children}
     </div>);
 };
